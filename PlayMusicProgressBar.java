f115c06003a71baed57f716b94f38c6642bc76fd
camillezapatos <camillezapatos@yahoo.com>
2016-02-15 20:17:39 -0500

    Update PlayMusicProgressBar.java

--- PlayMusicProgressBar.java
+++ PlayMusicProgressBar.java
@@ -17,12 +17,25 @@ public class PlayMusicProgressBar extends JFrame {
 		String path=fd.getDirectory();
 		String file=fd.getFile();
 		try {
-			FileReader fr=new FileReader(path+"//"+file);
+			FileReader fr=new FileReader(path+"C:\\temp\\korra.mp3"+file);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+	}
+	public void iterate(){
+		JProgressBar current;
+		Thread runner;
+		int num = 0;
+		while(num < 119000){
+			current.setValue(num);
+			try{
+				Thread.sleep(1000);
+			}catch (InterruptedException e)
+			{
+		}num += 100;
+	}
 	}
 
 }
